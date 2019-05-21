43d755f6c065ba57e4b1f5590ecf67297837a01b
Chris Brodt <chris@uberbrodt.net>
2019-05-21 08:09:54 -0500

    Updated to use MASTER on Horde

--- lib/test_horde/supervisor.ex
+++ lib/test_horde/supervisor.ex
@@ -3,7 +3,8 @@ defmodule TestHorde.Supervisor do
   use Horde.Supervisor
 
   def start_link do
-    Horde.Supervisor.start_link([name: __MODULE__, strategy: :one_for_one])
+    Horde.Supervisor.start_link([name: __MODULE__, strategy: :one_for_one, max_restart: 20,
+      max_seconds: 5 ])
   end
 
   def init(opts) do
--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule TestHorde.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:horde, "0.5.0"},
+      {:horde, github: "derekkraan/horde"}
       # {:horde, github: "derekkraan/horde"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
